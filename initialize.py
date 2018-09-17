760ddb07518a56bbd36e77b37dec9dacd3d5ebf3
Zixuan Wang <wzxllx@gmail.com>
2018-09-17 20:42:33 +0800

    Initialize considering passengers walk time.

--- initialize.py
+++ initialize.py
@@ -4,6 +4,7 @@ Initialize a random solution
 
 import Pucks
 import Gates
+import Tickets
 import checkFeasibility
 import numpy as np
 import compare_func
@@ -66,15 +67,53 @@ def initialize_greedy(pucks, gates):
 
     return allocation_result
 
+
+def initialize_for_passengers(pucks, gates, tickets):
+    '''
+    A greedy way to make passengers walk least
+    :param pucks:
+    :param gates:
+    :return: allocation matrix
+    '''
+
+    flight_names = set()
+
+    for p in pucks:
+        if p.arrive_flight[0] != '*':
+            flight_names.add(p.arrive_flight)
+        if p.depart_flight[0] != '*':
+            flight_names.add(p.depart_flight)
+
+    path_matrix = np.zeros((len(flight_names), len(flight_names)))
+
+    flight_names_arr = list(flight_names)
+
+    for t in tickets:
+        arr_f = flight_names_arr.index(t.arrive_flight)
+        dep_f = flight_names_arr.index(t.depart_flight)
+        path_matrix[arr_f][dep_f] += t.passengers_num
+
+    
+
+
+
+
+
+
+
 # main function
 if __name__ == "__main__":
     g = Gates.Gates().all_gates
     p = Pucks.Pucks(gates=g).all_pucks
+    t = Tickets.Tickets().all_tickets
 
     # result = initialize(p, g)
     # np.savetxt("result.csv", result, fmt="%d", delimiter=',')
     # print(checkFeasibility.check_feasibility(result, p, g))
 
-    result_greedy = initialize_greedy(p, g)
-    np.savetxt("result-greedy.csv", result_greedy, fmt="%d", delimiter=',')
-    print(checkFeasibility.check_feasibility(result_greedy, p, g))
+    # result_greedy = initialize_greedy(p, g)
+    # np.savetxt("result-greedy.csv", result_greedy, fmt="%d", delimiter=',')
+    # print(checkFeasibility.check_feasibility(result_greedy, p, g))
+
+    result_passengers = initialize_for_passengers(p, g, t)
+    print(1)
